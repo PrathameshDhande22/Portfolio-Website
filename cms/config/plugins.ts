6b642ff155d50eddc39ea8c635445724df5f28b2
Prathamesh Systenics <prathamesh.d@systenics.com>
2026-07-21 12:12:28 +0000

    AWS s3 plugin added and configuration

--- cms/config/plugins.ts
+++ cms/config/plugins.ts
@@ -33,12 +33,32 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin =>
   },
   upload: {
     config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        s3Options: {
+          credentials: {
+            accessKeyId: env('AWS_ACCESS_KEY_ID'),
+            secretAccessKey: env('AWS_ACCESS_SECRET'),
+          },
+          region: env('AWS_REGION'),
+          endpoint: env('AWS_ENDPOINT'),
+          forcePathStyle: true,
+          params: {
+            Bucket: env('AWS_BUCKET'),
+          },
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
       security: {
         allowedTypes: allowedMediaTypes,
         deniedTypes: deniedExecutableTypes,
-      },
-    },
-  },
+      }
+    }
+  }
 });
 
 export default config;
