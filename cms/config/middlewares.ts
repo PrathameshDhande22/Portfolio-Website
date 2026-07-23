eec4ca6e40b62ed860c0797494439ed465e75314
Prathameshdhande22 <prathameshdhande534@gmail.com>
2026-07-23 20:32:10 +0530

    feat: Added the SMTP and Configured the AWS MINIO Suitable view library

--- cms/config/middlewares.ts
+++ cms/config/middlewares.ts
@@ -1,9 +1,35 @@
 import type { Core } from '@strapi/strapi';
+import { env } from '@strapi/utils';
+
+const minioendpoint = env('AWS_ENDPOINT') 
 
 const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            minioendpoint,
+            'https://market-assets.strapi.io',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            minioendpoint,
+            'https://market-assets.strapi.io',
+          ],
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
--- cms/config/plugins.ts
+++ cms/config/plugins.ts
@@ -58,7 +58,25 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin =>
         deniedTypes: deniedExecutableTypes,
       }
     }
-  }
+  },
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST'),
+        port: env.int('SMTP_PORT'),
+        secure: false, // Use `true` for port 465
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+      },
+      settings: {
+        defaultFrom: 'no-reply@example.com',
+        defaultReplyTo: 'support@example.com',
+      },
+    },
+  },
 });
 
 export default config;
