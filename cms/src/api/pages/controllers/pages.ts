208a17b6ecc623b31873480b47c5288287e7014d
Prathameshdhande22 <prathameshdhande534@gmail.com>
2026-07-25 08:07:41 +0530

    feat: Added Pages Collection Type, Robots, Site Settings, Home Page, Skills and Skill Category Collections in Strapi

---
+++ cms/src/api/pages/controllers/pages.ts
@@ -0,0 +1,7 @@
+/**
+ * pages controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::pages.pages');
---
+++ cms/src/api/pages/routes/pages.ts
@@ -0,0 +1,7 @@
+/**
+ * pages router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::pages.pages');
