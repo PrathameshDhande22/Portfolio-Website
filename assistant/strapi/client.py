9862baf7626714f2ec5bcd88d22b830515008640
Prathameshdhande22 <prathameshdhande534@gmail.com>
2026-08-17 20:56:20 +0530

    refactor: Simplify API request handling by consolidating query parameters in StrapiClient methods

--- assistant/strapi/client.py
+++ assistant/strapi/client.py
@@ -1,6 +1,6 @@
 import logging
-from typing import List, Literal, Optional, TypeVar
-from httpx import AsyncClient
+from typing import List, Literal, Optional, TypeVar, Union
+from httpx import AsyncClient, QueryParams
 from pydantic import BaseModel
 from config import settings
 from models import (
@@ -34,17 +34,24 @@ class StrapiClient:
     def get_client(self):
         return self.client
 
-    async def get(self, endpoint: str, response_model: type[T] | BaseModel) -> T:
+    async def get(
+        self,
+        endpoint: str,
+        response_model: type[T] | BaseModel,
+        params: dict[str, Union[str, list[str]]] = None,
+    ) -> T:
         logger.info("Call the endpoint=%s", endpoint)
-        response = await self.client.get(endpoint)
+        query_params = QueryParams(params)
+        response = await self.client.get(endpoint, params=query_params)
         response.raise_for_status()
         return response_model.model_validate(response.json())
 
     async def get_model_settings(self) -> StrapiResponse[LLMSettings]:
-        response = await self.get(
-            "/llm-setting?populate=*", StrapiResponse[LLMSettings]
+        return await self.get(
+            "/llm-setting",
+            StrapiResponse[LLMSettings],
+            {"populate": "*"},
         )
-        return response
 
     async def get_skills(
         self,
@@ -52,81 +59,89 @@ class StrapiClient:
         name: Optional[str] = None,
     ) -> StrapiResponse[List[Skill]] | StrapiResponse[List[SkillCategory]]:
         if by == "Category":
-            qs = "/skill-categories?populate=*"
+            params = {"populate": "*"}
             if name:
-                qs += f"&filters[Name][$containsi]={name}"
-            return await self.get(qs, StrapiResponse[List[SkillCategory]])
+                params["filters[Name][$containsi]"] = name
+            return await self.get(
+                "/skill-categories", StrapiResponse[List[SkillCategory]], params
+            )
         else:
-            qs = "/skills?populate=*"
+            params = {"populate": "*"}
             if name:
-                qs += f"&filters[Name][$containsi]={name}"
-            return await self.get(qs, StrapiResponse[List[Skill]])
+                params["filters[Name][$containsi]"] = name
+            return await self.get("/skills", StrapiResponse[List[Skill]], params)
 
     async def get_projects(
         self,
         tag: Optional[str] = None,
         name: Optional[str] = None,
     ) -> StrapiResponse[List[Project]]:
-        qs = "/projects?populate[Tags][populate]=*&populate[Links][populate]=*"
+        params = {
+            "populate[Tags][populate]": "*",
+            "populate[Links][populate]": "*",
+        }
         if tag:
-            qs += f"&filters[Tags][Tag][$containsi]={tag}"
+            params["filters[Tags][Tag][$containsi]"] = tag
         if name:
-            qs += f"&filters[Title][$containsi]={name}"
-        return await self.get(qs, StrapiResponse[List[Project]])
+            params["filters[Title][$containsi]"] = name
+        return await self.get("/projects", StrapiResponse[List[Project]], params)
 
     async def get_education(self) -> StrapiResponse[List[Education]]:
         return await self.get(
-            "/educations?populate[Timeline][populate]=*",
+            "/educations",
             StrapiResponse[List[Education]],
+            {"populate[Timeline][populate]": "*"},
         )
 
     async def get_certifications(
         self,
         name: Optional[str] = None,
     ) -> StrapiResponse[List[Certification]]:
-        qs = "/certifications?populate[VerifyLink][populate]=*"
+        params = {"populate[VerifyLink][populate]": "*"}
         if name:
-            qs += f"&filters[$or][0][Title][$containsi]={name}"
-            qs += f"&filters[$or][1][Certifier][$containsi]={name}"
-        return await self.get(qs, StrapiResponse[List[Certification]])
+            params["filters[$or][0][Title][$containsi]"] = name
+            params["filters[$or][1][Certifier][$containsi]"] = name
+        return await self.get(
+            "/certifications", StrapiResponse[List[Certification]], params
+        )
 
     async def get_timeline(self) -> StrapiResponse[List[TimeLine]]:
         return await self.get(
-            "/time-lines?populate[Timeline][populate]=*",
+            "/time-lines",
             StrapiResponse[List[TimeLine]],
+            {"populate[Timeline][populate]": "*"},
         )
 
     async def get_experiences(
         self,
         name: Optional[str] = None,
     ) -> StrapiResponse[List[Experience]]:
-        qs = "/experiences?populate[Experience][populate]=*"
+        params = {"populate[Experience][populate]": "*"}
         if name:
-            qs += f"&filters[$or][0][Experience][Title][$containsi]={name}"
-            qs += f"&filters[$or][1][Experience][SubTitle][$containsi]={name}"
-        return await self.get(qs, StrapiResponse[List[Experience]])
+            params["filters[$or][0][Experience][Title][$containsi]"] = name
+            params["filters[$or][1][Experience][SubTitle][$containsi]"] = name
+        return await self.get("/experiences", StrapiResponse[List[Experience]], params)
 
     async def get_page(self, slug: str) -> StrapiResponse[List[Page]]:
-        qs = (
-            f"/page?filters[Slug][$eq]={slug}"
-            "&populate[SEO][populate]=*"
-            "&populate[Content][populate]=*"
-        )
-        return await self.get(qs, StrapiResponse[List[Page]])
+        params = {
+            "filters[Slug][$eq]": slug,
+            "populate[SEO][populate]": "*",
+            "populate[Content][populate]": "*",
+        }
+        return await self.get("/page", StrapiResponse[List[Page]], params)
 
     async def get_site_settings(self) -> StrapiResponse[SiteSettings]:
-        return await self.get(
-            "/site-setting"
-            "?populate[SocialLinks][populate]=*"
-            "&populate[Navigation][populate]=*"
-            "&populate[Footer][populate]=*"
-            "&populate[AskAI][populate]=*",
-            StrapiResponse[SiteSettings],
-        )
+        params = {
+            "populate[SocialLinks][populate]": "*",
+            "populate[Navigation][populate]": "*",
+            "populate[Footer][populate]": "*",
+            "populate[AskAI][populate]": "*",
+        }
+        return await self.get("/site-setting", StrapiResponse[SiteSettings], params)
 
     async def get_ai_knowledge(self) -> StrapiResponse[List[AIKnowledge]]:
         return await self.get(
-            "/ai-knowledges?populate=*", StrapiResponse[List[AIKnowledge]]
+            "/ai-knowledges", StrapiResponse[List[AIKnowledge]], {"populate": "*"}
         )
 
     async def close_client(self):
