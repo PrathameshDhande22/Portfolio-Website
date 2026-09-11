eb3f65246d2233fa2a2d034dd509ea23bf153a7b
Prathameshdhande22 <prathameshdhande534@gmail.com>
2026-09-12 00:09:52 +0530

    feat: update middleware configuration and enhance Traefik settings for Strapi CMS

--- cms/config/middlewares.ts
+++ cms/config/middlewares.ts
@@ -34,7 +34,17 @@ const config: Core.Config.Middlewares = [
   "strapi::cors",
   "strapi::poweredBy",
   "strapi::query",
-  "strapi::body",
+  {
+    name: 'strapi::body',
+    config: {
+      formLimit: '100mb',
+      jsonLimit: '100mb',
+      textLimit: '100mb',
+      formidable: {
+        maxFileSize: 100 * 1024 * 1024,
+      },
+    },
+  },
   "strapi::session",
   "strapi::favicon",
   "strapi::public",
